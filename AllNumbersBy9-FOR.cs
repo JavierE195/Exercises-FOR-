feebcba4331cb4b230db5f56be0c354e2efa4147
JavierE195 <ellerbrockjavier@hotmail.com>
2017-02-08 20:07:34 -0600

    Add files via upload

---
+++ AllNumbersBy9-FOR.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int i ;
+            int m = 1;
+
+            Console.Write("Insert a number: ");
+            N = int.Parse(Console.ReadLine());
+
+            for (i = 0; m < (N * 9); i++)
+            {
+                m = i * 9;
+                Console.Write("\n{0}", m);
+            }
+            Console.ReadKey();
+        }
+    }
+}
