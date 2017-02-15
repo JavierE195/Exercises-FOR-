5dcca4f136f9a3e05122a1ea0a0fba9b537cb264
JavierE195 <ellerbrockjavier@hotmail.com>
2017-02-14 20:00:19 -0600

    Add files via upload

---
+++ AstericsPiramid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 5;
+            int i;
+            int a;
+
+            for (i = 0; i < N; i++)
+            {
+                for (a = 0; a < i; a++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("*");
+            }
+            Console.ReadKey();
+        }
+    }
+}
