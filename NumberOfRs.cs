c63be499968500dd7c361ec5e7dc6398a96e15c4
JavierE195 <ellerbrockjavier@hotmail.com>
2017-02-14 22:34:44 -0600

    Add files via upload

---
+++ NumberOfRs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string b;
+            int c = 0;
+
+
+            b = Console.ReadLine();
+            for (int i = 0; i < b.Length; i++)
+            {
+                string r = b[i].ToString();
+                if (r == "r" || r == "R")
+                {
+                    c++;
+                }
+                 
+            }
+            Console.WriteLine(c);
+            
+            Console.ReadKey();
+        }
+    }
+}
